f8f3aaa52b1c8211b0aae892a755dc49e543f2dd
Matin Lotfaliei <matinlotfali@gmail.com>
2023-03-16 23:01:28 +0330

    Update build.sh

--- Build/build.sh
+++ Build/build.sh
@@ -3,4 +3,4 @@ git submodule sync
 git submodule update --init --recursive
 IMAGE_NAME="$(whoami)-rzv2l_vlp_v3.0.0"
 docker build -t ${IMAGE_NAME}:latest .
-docker rmi $(docker images | grep "^<none" | awk '{print $3}')
+docker images | grep "^<none" | awk '{print $3}' | xargs docker rmi
