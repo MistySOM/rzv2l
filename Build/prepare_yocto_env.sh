9cb99a36ff0f3bcc100ae7ea5b0d8089769dd540
Ron Eggler <ron.eggler@mistywest.com>
2023-11-03 17:16:03 -0700

    switched submodule to the vlp 3.0.4 branch and changed the archive downloads

--- Build/prepare_yocto_env.sh
+++ Build/prepare_yocto_env.sh
@@ -16,5 +16,5 @@ tar zxvf ~/RTK0EF0045Z13001ZJ-v1.1.0_EN/meta-rz-features_graphics_v1.1.0.tar.gz 
 unzip -o ~/RTK0EF0045Z15001ZJ-v1.1.0_EN.zip -d ~
 tar zxvf ~/RTK0EF0045Z15001ZJ-v1.1.0_EN/meta-rz-features_codec_v1.1.0.tar.gz --no-same-owner
 
-unzip -o ~/r11an0549ej0740-rzv2l-drpai-sp.zip -d ~
-tar xvf ~/rzv2l_drpai-driver/meta-rz-drpai.tar.gz -C /home/yocto/rzv_vlp_v3.0.4/ --no-same-owner
+unzip -o ~/r11an0549ej0740-rzv2l-drpai-sp.zip -d ~/r11an0549ej0740-rzv2l-drpai-sp
+tar xvf ~/r11an0549ej0740-rzv2l-drpai-sp/rzv2l_drpai-driver/meta-rz-drpai.tar.gz -C /home/yocto/rzv_vlp_v3.0.4/ --no-same-owner
