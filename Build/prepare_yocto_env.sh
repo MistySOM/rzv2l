0d12bce7c6b3c02a63421a0db347769040c3adb9
Matin Lotfaliei <matinlotfali@gmail.com>
2024-02-27 15:19:26 -0800

    Add cleanup to prepare_yocto_env

--- Build/prepare_yocto_env.sh
+++ Build/prepare_yocto_env.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# download
 wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.4/RTK0EF0045Z0024AZJ-v3.0.4.zip # BSP
 wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.4/RTK0EF0045Z13001ZJ-v1.1.0_EN.zip # Graphics package Evaluation version
 wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.4/RTK0EF0045Z15001ZJ-v1.1.0_EN.zip # VIDEO CODEC PACKAGE Evaluation version
 wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.4/r11an0549ej0740-rzv2l-drpai-sp.zip # DRPAI stuff for V2L
 
+# extract
 cd $WORK || exit 1
 
 unzip -o ~/RTK0EF0045Z0024AZJ-v3.0.4.zip -d ~
@@ -18,3 +20,16 @@ tar zxvf ~/RTK0EF0045Z15001ZJ-v1.1.0_EN/meta-rz-features_codec_v1.1.0.tar.gz --n
 
 unzip -o ~/r11an0549ej0740-rzv2l-drpai-sp.zip -d ~/r11an0549ej0740-rzv2l-drpai-sp
 tar xvf ~/r11an0549ej0740-rzv2l-drpai-sp/rzv2l_drpai-driver/meta-rz-drpai.tar.gz -C /home/yocto/rzv_vlp_v3.0.4/ --no-same-owner
+
+# clean up
+echo "Cleaning Up..."
+rm -rf ~/RTK0EF0045Z0024AZJ-v3.0.4.zip
+rm -rf ~/RTK0EF0045Z0024AZJ-v3.0.4
+rm -rf ~/RTK0EF0045Z13001ZJ-v1.1.0_EN.zip
+rm -rf ~/RTK0EF0045Z13001ZJ-v1.1.0_EN
+rm -rf ~/RTK0EF0045Z15001ZJ-v1.1.0_EN.zip
+rm -rf ~/RTK0EF0045Z15001ZJ-v1.1.0_EN
+rm -rf ~/r11an0549ej0740-rzv2l-drpai-sp.zip
+rm -rf ~/r11an0549ej0740-rzv2l-drpai-sp
+
+echo "Wrapping Layer..."
