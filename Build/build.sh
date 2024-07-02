5255517a45c7eefa3f6eca1f362bdac92589ffa7
MistyRon <ron.eggler@mistywest.com>
2024-07-01 19:14:29 -0700

    Added help to build script and introduced BSP version variable

--- Build/build.sh
+++ Build/build.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 NAME="rzv2l_vlp_v3.0.4"
-if [ "$1" == "-b" ]; then
+Help()
+{
+   echo "This script createss the build container for MistySOM G2L or V2L."
+   echo
+   echo "Syntax: $0 [-h|-b]"
+   echo "options:"
+   echo "h     Show this help."
+   echo "b     Include current branch name in container name."
+   echo
+}
+# Get the options
+while getopts ":hb" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      b) # include branch name
+         INCLUDE_BRANCH=1;;
+     \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+if [ $INCLUDE_BRANCH -eq 1 ]; then
 	IMAGE_NAME="$(whoami)-${NAME}_$(git branch --show-current)"
 else
 	IMAGE_NAME="$(whoami)-${NAME}"
