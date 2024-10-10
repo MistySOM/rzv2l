3167f9e9f65e4efcf5876f105c241ed49eaebf64
Ron Eggler <ron.eggler@mistywest.com>
2024-10-10 09:12:34 -0700

    Merge master into Projects/MH11C-MistyVision

--- Build/prepare_yocto_env.sh
+++ Build/prepare_yocto_env.sh
@@ -1,22 +1,35 @@
+#!/bin/sh
 
-#RUN wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.0/oss_pkg_rzv_v3.0.0.7z
-wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.0/rzv_bsp_v3.0.0.tar.gz
-wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.0/RTK0EF0045Z13001ZJ-v1.2_EN.zip
-wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.0/RTK0EF0045Z15001ZJ-v0.58_EN.zip
-wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.0/RTK0EF0045Z0024AZJ-v3.0.0-update2.zip
-wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.0/r11an0549ej0720-rzv2l-drpai-sp.zip
+# download
+wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.4/RTK0EF0045Z0024AZJ-v3.0.4.zip # BSP
+wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.4/RTK0EF0045Z13001ZJ-v1.1.0_EN.zip # Graphics package Evaluation version
+wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.4/RTK0EF0045Z15001ZJ-v1.1.0_EN.zip # VIDEO CODEC PACKAGE Evaluation version
+wget https://remote.mistywest.com/download/mh11/rzv2l/VerifiedLinuxPackage_v3.0.4/r11an0549ej0740-rzv2l-drpai-sp.zip # DRPAI stuff for V2L
 
+# extract
 cd $WORK || exit 1
 
-unzip -o ~/RTK0EF0045Z0024AZJ-v3.0.0-update2.zip -d ~
-tar zxvf ~/RTK0EF0045Z0024AZJ-v3.0.0-update2/rzv_bsp_v3.0.0.tar.gz --no-same-owner
-patch -p1 < ~/RTK0EF0045Z0024AZJ-v3.0.0-update2/rzv_v300-to-v300update2.patch
+unzip -o ~/RTK0EF0045Z0024AZJ-v3.0.4.zip -d ~
+tar zxvf ~/RTK0EF0045Z0024AZJ-v3.0.4/rzv_vlp_v3.0.4.tar.gz --no-same-owner
 
-unzip -o ~/RTK0EF0045Z13001ZJ-v1.2_EN.zip -d ~
-tar zxvf ~/RTK0EF0045Z13001ZJ-v1.2_EN/meta-rz-features.tar.gz --no-same-owner
+unzip -o ~/RTK0EF0045Z13001ZJ-v1.1.0_EN.zip -d ~
+tar zxvf ~/RTK0EF0045Z13001ZJ-v1.1.0_EN/meta-rz-features_graphics_v1.1.0.tar.gz --no-same-owner
 
-unzip -o ~/RTK0EF0045Z15001ZJ-v0.58_EN.zip -d ~
-tar zxvf ~/RTK0EF0045Z15001ZJ-v0.58_EN/meta-rz-features.tar.gz --no-same-owner
+unzip -o ~/RTK0EF0045Z15001ZJ-v1.1.0_EN.zip -d ~
+tar zxvf ~/RTK0EF0045Z15001ZJ-v1.1.0_EN/meta-rz-features_codec_v1.1.0.tar.gz --no-same-owner
 
-unzip -o ~/r11an0549ej0720-rzv2l-drpai-sp.zip -d ~/r11an0549ej0720-rzv2l-drpai-sp
-tar zxvf ~/r11an0549ej0720-rzv2l-drpai-sp/rzv2l_drpai-driver/meta-rz-features.tar.gz --no-same-owner
+unzip -o ~/r11an0549ej0740-rzv2l-drpai-sp.zip -d ~/r11an0549ej0740-rzv2l-drpai-sp
+tar xvf ~/r11an0549ej0740-rzv2l-drpai-sp/rzv2l_drpai-driver/meta-rz-drpai.tar.gz -C /home/yocto/rzv_vlp_v3.0.4/ --no-same-owner
+
+# clean up
+echo "Cleaning Up..."
+rm -rf ~/RTK0EF0045Z0024AZJ-v3.0.4.zip
+rm -rf ~/RTK0EF0045Z0024AZJ-v3.0.4
+rm -rf ~/RTK0EF0045Z13001ZJ-v1.1.0_EN.zip
+rm -rf ~/RTK0EF0045Z13001ZJ-v1.1.0_EN
+rm -rf ~/RTK0EF0045Z15001ZJ-v1.1.0_EN.zip
+rm -rf ~/RTK0EF0045Z15001ZJ-v1.1.0_EN
+rm -rf ~/r11an0549ej0740-rzv2l-drpai-sp.zip
+rm -rf ~/r11an0549ej0740-rzv2l-drpai-sp
+
+echo "Wrapping Layer..."
